7ba8d2225d46905569d682e0cbad7f31131ae8c2
abhinav3254 <abhinav3254@gmail.com>
2023-11-10 20:36:59 +0530

    other functions

--- chapterone/important_functions.py
+++ chapterone/important_functions.py
@@ -7,6 +7,16 @@ img = cv2.imread("res/krati.png")
 # this cvtColor() will convert our image into various color spaces
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-cv2.imshow("window_name",imgGray)
+# making image blur
+# (7,7) is our knumber -> kernal number it can be odd numbers like 3x3 or 7x7 etc
+# sigmaX is 0
+imgBlur = cv2.GaussianBlur(img,(7,7),0)
 
+# canny
+imgCanny = cv2.Canny(img,150,200)
+
+cv2.imshow("original_image",img)
+cv2.imshow("window_name",imgGray)
+cv2.imshow("blur_img",imgBlur)
+cv2.imshow("canny_img",imgCanny)
 cv2.waitKey(0)
