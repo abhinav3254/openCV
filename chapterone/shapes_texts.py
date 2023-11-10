48bd9cfe01e7c196040390eee703dc9012f88256
abhinav3254 <abhinav3254@gmail.com>
2023-11-10 22:43:27 +0530

    creating line

--- chapterone/shapes_texts.py
+++ chapterone/shapes_texts.py
@@ -21,6 +21,16 @@ cv2.imshow("image_window",img)
 
 '''
 
+# now we going to create lines
+img = np.zeros((256,256,3))
 
+
+# here we are creating a line
+# (0,0) is the starting point from where we are going to start
+# (200,200) is the ending point upto which we are going to end
+# and (0,255,0) is the color code for the line
+cv2.line(img,(0,0),(200,200),(0,255,0),3)
+
+cv2.imshow("img_window",img)
 
 cv2.waitKey(0)
