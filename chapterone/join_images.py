261dc07575698354c832c2ab2729da646e4648d3
abhinav3254 <abhinav3254@gmail.com>
2023-11-10 23:32:47 +0530

    join two images

--- chapterone/join_images.py
+++ chapterone/join_images.py
@@ -3,6 +3,8 @@ import numpy as np
 
 img = cv2.imread("res/brock.png")
 
+
+'''
 # adding both images side by side
 imgHor = np.hstack((img,img))
 
@@ -14,5 +16,9 @@ cv2.imshow("window_vert",imgVertical)
 # issue with this way of joining two images
 # 1. Both images must have same channels if they don't have same number of channels then we can't join both of them
 # 2. This way we can't resize the image
+
+'''
+
+# solution for the above problem
 
 cv2.waitKey(0)
